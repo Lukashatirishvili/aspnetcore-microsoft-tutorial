5908d69ea80fcf7027cd95cecb8152615dc662c5
Luka Shatirishvili <lukkashatirishvili@gmail.com>
2026-05-15 22:13:48 +0400

    Added - Search functionality

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MoviesApp.Data;
 using MoviesApp.Models;
@@ -20,10 +21,46 @@ public class MoviesController : Controller
     }
     
     // GET
-    public async Task<IActionResult> Index()
+    // Search
+    // GET: Movies
+    public async Task<IActionResult> Index(string movieGenre, string searchString)
     {
-        var movie = await _context.Movies.ToListAsync();
-        return View(movie);
+        if (_context.Movies == null)
+        {
+            return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+        }
+
+        // Use LINQ to get list of genres.
+        IQueryable<string> genreQuery = from m in _context.Movies
+            orderby m.Genre
+            select m.Genre;
+        
+        var movies = from m in _context.Movies
+            select m;
+
+        if (!string.IsNullOrEmpty(searchString))
+        {
+            movies = movies.Where(s => s.Title!.ToUpper().Contains(searchString.ToUpper()));
+        }
+
+        if (!string.IsNullOrEmpty(movieGenre))
+        {
+            movies = movies.Where(x => x.Genre == movieGenre);
+        }
+
+        var movieGenreVM = new MovieGenreViewModel
+        {
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+            Movies = await movies.ToListAsync()
+        };
+
+        return View(movieGenreVM);
+    }
+    
+    [HttpPost]
+    public string Index(string searchString, bool notUsed)
+    {
+        return "From [HttpPost]Index: filter on " + searchString;
     }
     
     // Edit GET method
@@ -94,4 +131,7 @@ public class MoviesController : Controller
         
         return View(movie);
     }
+    
+    
+
 }
